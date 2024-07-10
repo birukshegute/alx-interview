35bbb9cb34123dfadcc79b34c56ba62687284a98
birukshegute <birukshegute@gmail.com>
2024-07-10 13:41:46 +0000

    task 0

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+performs minOperations(n) function
+"""
+
+
+def minOperations(n):
+    """
+    a method that calculates the fewest number of operations
+    needed to result in exactly n H characters in the file.
+    """
+    next = 'H'
+    file = 'H'
+    operations = 0
+    while (len(file) < n):
+        if n % len(file) == 0:
+            operations += 2
+            next = file
+            file += file
+        else:
+            operations += 1
+            file += next
+    if len(file) != n:
+        return 0
+    return operations
