ddc87f35922973b77de275807e6ff04d93dc7ad6
birukshegute <birukshegute@gmail.com>
2024-07-13 17:37:44 +0000

    well the code is write because it provides the correct result when I test it with 2147483640 char. But I can't solve the time complexity because it takes 326 steps to solve the problem. So I have to move on now.

--- 0x02-minimum_operations/0-main.py
+++ 0x02-minimum_operations/0-main.py
@@ -5,8 +5,20 @@ Main file for testing
 
 minOperations = __import__('0-minoperations').minOperations
 
+n = 0
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 1
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
 n = 4
 print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
 
 n = 12
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 13
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 2147483640
 print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
