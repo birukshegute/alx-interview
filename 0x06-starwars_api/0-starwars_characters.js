8954fb27ba7aab6941c1a0f88270b3c5f4e0bf65
birukshegute <birukshegute@gmail.com>
2024-08-08 23:30:15 +0300

    trial 1

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const query = (arr, i) => {
+  if (i === arr.length) return;
+  request(arr[i], (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log(JSON.parse(body).name);
+      query(arr, i + 1);
+    }
+  });
+};
+
+request(
+  'https://swapi-api.alx-tools.com/api/films/${process.argv[2]}',
+  (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      const chars = JSON.parse(body).characters;
+      query(chars, 0);
+    }
+  }
+);
